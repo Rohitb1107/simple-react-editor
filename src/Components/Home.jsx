c3373a643bc6c9234c99889d286cb99fd4578ead
Rohit <stylishrb711@gmail.com>
2022-09-26 19:19:03 +0530

    app update!

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Highlight, {defaultProps} from "prism-react-renderer";
+import theme from 'prism-react-renderer/themes/vsDark'
+
+const Home = () => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Home;
